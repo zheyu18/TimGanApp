3400f3614d613b7e1626b90c49898583dd75d2c7
zheyu <3301360040@qq.com>
2021-01-22 15:40:07 +0800

    完善代码

--- app/src/main/java/com/example/timganapp/ui/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/timganapp/ui/adapter/MainAdapter.kt
@@ -1,31 +1,14 @@
-/*
- * Created by 动脑科技-Tim on 17-6-20 下午10:37
- * Copyright (c) 2017. All rights reserved
- *
- * Last modified 17-6-20 下午10:37
- */
-
 package com.example.timganapp.ui.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
+class MainAdapter(var data:List<Fragment> = ArrayList(),var titles:List<String>,fm:FragmentManager):FragmentStatePagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment = data[position]
 
-class MainAdapter (var data:List<Fragment> = ArrayList(), var titles: Array<String>, fm: FragmentManager):
-FragmentStatePagerAdapter(fm){
-    override fun getItem(position: Int): Fragment {
-        return data[position]
-    }
+    override fun getCount(): Int = data.size
 
-    override fun getCount(): Int {
-
-        return data.size
-    }
-
-    override fun getPageTitle(position: Int): CharSequence {
-        return titles[position]
-    }
-
+    override fun getPageTitle(position: Int): CharSequence? = titles[position]
 
 }
